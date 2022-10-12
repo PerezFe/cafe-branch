b5d5bffb810da674eefc26f287bf2aee0eed0bac
PerezFe <PerezFernandoE83@gmail.com>
2022-10-12 20:47:40 -0300

    creado los componentes necesarios

---
+++ src/component/views/Administrador.jsx
@@ -0,0 +1,10 @@
+
+const Administrador = () => {
+    return (
+        <div>
+            <h2>Pagina del administrador</h2>
+        </div>
+    );
+};
+
+export default Administrador;
---
+++ src/component/views/Error404.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Error404 = () => {
+    return (
+        <div>
+            <h3>Pagina de error</h3>
+        </div>
+    );
+};
+
+export default Error404;
