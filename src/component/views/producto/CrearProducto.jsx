ecdc243d69c9dcc23ccdde15acf49fbc2a2e877a
PerezFe <PerezFernandoE83@gmail.com>
2022-10-20 15:18:47 -0300

    agregamos la peticion para crear un producto

--- src/component/views/producto/CrearProducto.jsx
+++ src/component/views/producto/CrearProducto.jsx
@@ -1,20 +1,46 @@
 import { Form, Button } from "react-bootstrap";
-import {useForm} from "react-hook-form"
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { crearProductoAPI } from "../../helpers/queries";
+import { useNavigate } from "react-router-dom";
 
 const CrearProducto = () => {
-  const {register, handleSubmit, formState:{errors}} = useForm(
-    {defaultValues:{
-      nombreProducto:"",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm({
+    defaultValues: {
+      nombreProducto: "",
       precio: 1,
       imagen: "",
-      categoria:""
+      categoria: "",
     },
   });
+  //inicializar useNavigate
+  const navegacion = useNavigate();
 
-  const onSubmit = (datos)=>{
-    console.log(datos)
-    console.log("desde nuestra funcion submit")
-  }
+  const onSubmit = (datos) => {
+    console.log(datos);
+    // enviar los datos de la api
+    crearProductoAPI(datos).then((respuesta) => {
+      if (respuesta.status === 201) {
+        //el producto se creo
+        Swal.fire(
+          "Producto creado",
+          "El producto fue creado correctamente",
+          "success"
+        );
+        reset();
+        //redireccionar
+        navegacion("/administrador");
+      } else {
+        //mostrar mensaje de error al usuario
+        Swal.fire("Ocurrio un error ", "Intentelo mas tarde", "error");
+      }
+    });
+  };
 
   return (
     <section className="container mainSection">
@@ -24,20 +50,20 @@ const CrearProducto = () => {
       <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formNombreProdcuto">
           <Form.Label>Nombre producto*</Form.Label>
-          <Form.Control 
-          type="text" 
-          placeholder="Ej: Cafe"
-          {...register("nombreProducto", {
-            required:"Este dato es obligatorio",
-            minLength:{
-              value:2,
-              message:"Debe ingresar como minimo 2 caracteres"
-            },
-            maxLength:{
-              value:50,
-              message:"Debe ingresar como maximo 20 caracteres"
-            } 
-          })}
+          <Form.Control
+            type="text"
+            placeholder="Ej: Cafe"
+            {...register("nombreProducto", {
+              required: "Este dato es obligatorio",
+              minLength: {
+                value: 2,
+                message: "Debe ingresar como minimo 2 caracteres",
+              },
+              maxLength: {
+                value: 50,
+                message: "Debe ingresar como maximo 20 caracteres",
+              },
+            })}
           />
           <Form.Text className="text-danger">
             {errors.nombreProducto?.message}
@@ -45,47 +71,58 @@ const CrearProducto = () => {
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Precio*</Form.Label>
-          <Form.Control type="number" placeholder="Ej: 50"
-          {...register("precio",{
-            required: "El precio es un valor requerido",
-            min:{
-              value: 1,
-              message: "El precio debe ser como minimo de $1"
-            },
-            max:{
-              value: 1000,
-              message: "El precio de producto como maximo debe ser de 10000"
-            }
-          })} />
-          <Form.Text className="text-danger">{errors.precio?.message}</Form.Text>
+          <Form.Control
+            type="number"
+            placeholder="Ej: 50"
+            {...register("precio", {
+              required: "El precio es un valor requerido",
+              min: {
+                value: 1,
+                message: "El precio debe ser como minimo de $1",
+              },
+              max: {
+                value: 1000,
+                message: "El precio de producto como maximo debe ser de 10000",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.precio?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Imagen URL*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: https://www.pexels.com/es-es/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/"
-            {...register("imagen",{
-              required:"La url de la imagen es obligatoria",
-              pattern:{
-                value:/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/,
-                message: "Debe ingresar una URL válida"
-              }
+            {...register("imagen", {
+              required: "La url de la imagen es obligatoria",
+              pattern: {
+                value: /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/,
+                message: "Debe ingresar una URL válida",
+              },
             })}
           />
-          <Form.Text className="text-danger">{errors.imagen?.message}</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.imagen?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Categoria*</Form.Label>
-          <Form.Select {...register("categoria",{
-            required:"Debe seleccionar una categoría",
-          })}>
+          <Form.Select
+            {...register("categoria", {
+              required: "Debe seleccionar una categoría",
+            })}
+          >
             <option value="">Seleccione una opcion</option>
             <option value="bebida caliente">Bebida caliente</option>
             <option value="bebida fria">Bebida fria</option>
             <option value="dulce">Dulce</option>
             <option value="salado">Salado</option>
           </Form.Select>
-          <Form.Text className="text-danger">{errors.categoria?.message}</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.categoria?.message}
+          </Form.Text>
         </Form.Group>
         <Button variant="primary" type="submit">
           Guardar
